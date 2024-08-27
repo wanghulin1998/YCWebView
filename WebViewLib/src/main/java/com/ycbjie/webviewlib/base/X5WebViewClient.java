65d6e0356a97270cc307ceec24cb3204d47e092b
whl <656385141@qq.com>
2024-08-27 10:54:54 +0800

    1.5.7

--- WebViewLib/src/main/java/com/ycbjie/webviewlib/base/X5WebViewClient.java
+++ WebViewLib/src/main/java/com/ycbjie/webviewlib/base/X5WebViewClient.java
@@ -372,7 +372,7 @@ public class X5WebViewClient extends WebViewClient {
         }
         //html加载完成之后，添加监听图片的点击js函数
         //addImageClickListener();
-        addImageArrayClickListener(webView);
+//        addImageArrayClickListener(webView);
         isLoadFinish = true;
     }
 
